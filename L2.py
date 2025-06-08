296d828ffaa1e608603f0e73e157470a47537a65
DishaV345 <dishavarun01@gmail.com>
2025-06-08 12:21:09 +0530

    add no.

--- L2.py
+++ L2.py
@@ -1,7 +1,7 @@
 #slicing
 #ending ind not included
 str= "apna college bish "
-print(str[0:len(str)]) #indexing = locant no.
+print(str[0:4]) #indexing = locant no.
 
 #negative indexing
 print(str[-12:-1])
