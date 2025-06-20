527a3843f257efc6d6b514d61ec73442ca336cdf
DishaV345 <dishavarun01@gmail.com>
2025-06-20 08:25:11 +0530

    new file added

--- L5.py
+++ L5.py
@@ -1,7 +1,7 @@
 #LOOPS -> repeats instructions
 #-> 2 types
 
-i = 1
+i = 1 #iterator -> constant used for looping
 while i <= 7 :
-    print("yo yo honey")
+    print("yo yo honey") #iteration -> no. of cycles
     i = i+1
